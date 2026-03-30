a0a0bd0628ffbc8eb2b27ca27e5afff6ca827e4d
singhrishav1009 <singhrishav1009@gmail.com>
2026-03-30 12:58:22 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+    // 1st Approach
+
+    unordered_map <char,int> ans;
+
+    for (int i=0;i<s.length();i++){
+        ans[s[i]]++;
+    }
+
+    for (int i=0;i<t.length();i++){
+        ans[t[i]]--;
+    }
+
+    for(auto &p : ans) {
+        // check the value not key
+        if(p.second!=0){
+            return false;
+        }
+    }
+    
+    return true;
+
+    // 2nd approach
+    // unordered_map <char,int> ans_s;
+    // unordered_map <char,int> ans_t;
+
+    // for (int i=0;i<s.length();i++){
+    //     ans_s[s[i]]++;
+    // }
+
+    // for (int i=0;i<t.length();i++){
+    //     ans_t[t[i]]++;
+    // }
+
+    // return ans_s == ans_t;
+
+
+    // Best Approach
+    // unordered_map<char, int> countS;
+    // unordered_map<char, int> countT;
+    // for (int i = 0; i < s.length(); i++) {
+    //     countS[s[i]]++;
+    //     countT[t[i]]++;
+    // }
+    // return countS == countT;
+
+};
+
+};
